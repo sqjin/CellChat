cd3e5b3d28d9e285099ce80d0d7a5e9c64937bbe
sqjin <32399212+sqjin@users.noreply.github.com>
2020-08-23 08:09:14 -0700

    circle plot supports showing partial connections

--- R/visualization.R
+++ R/visualization.R
@@ -58,6 +58,10 @@ scPalette <- function(n) {
 #' @param signaling.name alternative signaling pathway name to show on the plot
 #' @param vertex.receiver a numeric vector giving the index of the cell groups as targets in the first hierarchy plot
 #' @param color.use the character vector defining the color of each cell group
+#' @param from a vector giving the index or the name of source cell groups when using circle plot
+#' @param to a vector giving the index or the name of target cell groups.
+#' @param bidirection whether show the bidirectional communication, i.e., both 'from'->'to' and 'to'->'from'.
+#' @param remove.isolate whether remove the isolate nodes in the communication network
 #' @param vertex.size The size of vertex
 #' @param layout "hierarchy" or "circle"
 #' @param height height of plot
@@ -73,7 +77,7 @@ scPalette <- function(n) {
 #'
 #' @examples
 #'
-netVisual <- function(object, signaling, signaling.name = NULL, vertex.receiver = NULL, color.use = NULL, vertex.size = 20, layout = c("hierarchy","circle"), height = 5, thresh = 0.05, pt.title = 12, title.space = 6, vertex.label.cex = 0.8) {
+netVisual <- function(object, signaling, signaling.name = NULL, vertex.receiver = NULL, color.use = NULL, from = NULL, to = NULL, bidirection = FALSE, remove.isolate = FALSE, vertex.size = 20, layout = c("hierarchy","circle"), height = 5, thresh = 0.05, pt.title = 12, title.space = 6, vertex.label.cex = 0.8) {
   layout <- match.arg(layout)
   pairLR <- searchPair(signaling = signaling, pairLR.use = object@LR$LRsig, key = "pathway_name", matching.exact = T, pair.only = F)
 
@@ -139,18 +143,16 @@ netVisual <- function(object, signaling, signaling.name = NULL, vertex.receiver 
       #signalName_i <- paste0(pairLR$ligand[i], "-",pairLR$receptor[i], sep = "")
       signalName_i <- pairLR$interaction_name_2[i]
       prob.i <- prob[,,i]
-      netVisual_circle(prob.i, top = 1, color.use = color.use, vertex.size = vertex.size, signaling.name = signalName_i, vertex.label.cex = vertex.label.cex)
+      netVisual_circle(prob.i, top = 1, color.use = color.use, from = from, to = to, bidirection = bidirection, remove.isolate = remove.isolate, vertex.size = vertex.size, signaling.name = signalName_i, vertex.label.cex = vertex.label.cex)
     }
     dev.off()
 
     prob.sum <- apply(prob, c(1,2), sum)
     prob.sum <-(prob.sum-min(prob.sum))/(max(prob.sum)-min(prob.sum))
     svglite(file = paste0(signaling.name,"_", layout,  "_aggregate.svg"), width = height, height = 1*height)
-    netVisual_circle(prob.sum, top = 1, color.use = color.use, vertex.size = vertex.size, signaling.name = paste0(signaling.name, " signaling pathway network"), vertex.label.cex = vertex.label.cex)
+    netVisual_circle(prob.sum, top = 1, color.use = color.use, from = from, to = to, bidirection = bidirection, remove.isolate = remove.isolate, vertex.size = vertex.size, signaling.name = paste0(signaling.name, " signaling pathway network"), vertex.label.cex = vertex.label.cex)
     dev.off()
   }
-
-
 }
 
 
@@ -161,6 +163,10 @@ netVisual <- function(object, signaling, signaling.name = NULL, vertex.receiver 
 #' @param signaling.name alternative signaling pathway name to show on the plot
 #' @param vertex.receiver a numeric vector giving the index of the cell groups as targets in the first hierarchy plot
 #' @param color.use the character vector defining the color of each cell group
+#' @param from a vector giving the index or the name of source cell groups when using circle plot
+#' @param to a vector giving the index or the name of target cell groups.
+#' @param bidirection whether show the bidirectional communication, i.e., both 'from'->'to' and 'to'->'from'.
+#' @param remove.isolate whether remove the isolate nodes in the communication network
 #' @param vertex.size The size of vertex
 #' @param layout "hierarchy" or "circle"
 #' @param thresh threshold of the p-value for determining significant interaction
@@ -174,7 +180,7 @@ netVisual <- function(object, signaling, signaling.name = NULL, vertex.receiver 
 #'
 #' @examples
 #'
-netVisual_aggregate <- function(object, signaling, signaling.name = NULL, vertex.receiver = NULL, color.use = NULL, vertex.size = 20, layout = c("hierarchy","circle"), thresh = 0.05,
+netVisual_aggregate <- function(object, signaling, signaling.name = NULL, vertex.receiver = NULL, color.use = NULL, from = NULL, to = NULL, bidirection = FALSE, remove.isolate = FALSE, vertex.size = 20, layout = c("hierarchy","circle"), thresh = 0.05,
                                 pt.title = 12, title.space = 6, vertex.label.cex = 0.8) {
   layout <- match.arg(layout)
   pairLR <- searchPair(signaling = signaling, pairLR.use = object@LR$LRsig, key = "pathway_name", matching.exact = T, pair.only = T)
@@ -224,7 +230,7 @@ netVisual_aggregate <- function(object, signaling, signaling.name = NULL, vertex
   } else if (layout == "circle") {
     prob.sum <- apply(prob, c(1,2), sum)
     prob.sum <-(prob.sum-min(prob.sum))/(max(prob.sum)-min(prob.sum))
-    netVisual_circle(prob.sum, top = 1, color.use = color.use, vertex.size = vertex.size, signaling.name = paste0(signaling.name, " signaling pathway network"), vertex.label.cex = vertex.label.cex)
+    netVisual_circle(prob.sum, top = 1, color.use = color.use, from = from, to = to, bidirection = bidirection, remove.isolate = remove.isolate, vertex.size = vertex.size, signaling.name = paste0(signaling.name, " signaling pathway network"), vertex.label.cex = vertex.label.cex)
   }
 
 
@@ -239,6 +245,10 @@ netVisual_aggregate <- function(object, signaling, signaling.name = NULL, vertex
 #' @param signaling.name alternative signaling pathway name to show on the plot
 #' @param vertex.receiver a numeric vector giving the index of the cell groups as targets in the first hierarchy plot
 #' @param color.use the character vector defining the color of each cell group
+#' @param from a vector giving the index or the name of source cell groups when using circle plot
+#' @param to a vector giving the index or the name of target cell groups.
+#' @param bidirection whether show the bidirectional communication, i.e., both 'from'->'to' and 'to'->'from'.
+#' @param remove.isolate whether remove the isolate nodes in the communication network
 #' @param vertex.size The size of vertex
 #' @param layout "hierarchy" or "circle"
 #' @param height height of plot
@@ -250,9 +260,9 @@ netVisual_aggregate <- function(object, signaling, signaling.name = NULL, vertex
 #'
 #' @examples
 #'
-netVisual_individual <- function(object, signaling, signaling.name = NULL, vertex.receiver = NULL, color.use = NULL, vertex.size = 20, layout = c("hierarchy","circle"), height = 5, thresh = 0.05) {
+netVisual_individual <- function(object, signaling, signaling.name = NULL, vertex.receiver = NULL, color.use = NULL, from = NULL, to = NULL, bidirection = FALSE, remove.isolate = FALSE, vertex.size = 20, layout = c("hierarchy","circle"), height = 5, thresh = 0.05) {
   layout <- match.arg(layout)
-  pairLR <- searchPair(signaling = signaling, pairLR.use = object@LR$LRsig, key = "pathway_name", matching.exact = T, pair.only = T)
+  pairLR <- searchPair(signaling = signaling, pairLR.use = object@LR$LRsig, key = "pathway_name", matching.exact = T, pair.only = F)
 
   if (is.null(signaling.name)) {
     signaling.name <- signaling
@@ -293,7 +303,7 @@ netVisual_individual <- function(object, signaling, signaling.name = NULL, verte
   if (layout == "hierarchy") {
     par(mfrow=c(nRow,2), mar = c(5, 4, 4, 2) +0.1)
     for (i in 1:length(pairLR.name.use)) {
-      signalName_i <- paste0(pairLR$ligand[i], "-",pairLR$receptor[i], sep = "")
+      signalName_i <- pairLR$interaction_name_2[i]
       prob.i <- prob[,,i]
       netVisual_hierarchy1(prob.i, vertex.receiver = vertex.receiver, color.use = color.use, vertex.size = vertex.size, signaling.name = signalName_i)
       netVisual_hierarchy2(prob.i, vertex.receiver = setdiff(1:nrow(prob.i),vertex.receiver), color.use = color.use, vertex.size = vertex.size, signaling.name = signalName_i)
@@ -302,14 +312,13 @@ netVisual_individual <- function(object, signaling, signaling.name = NULL, verte
   } else if (layout == "circle") {
     par(mfrow=c(nRow,1))
     for (i in 1:length(pairLR.name.use)) {
-      signalName_i <- paste0(pairLR$ligand[i], "-",pairLR$receptor[i], sep = "")
+      signalName_i <- pairLR$interaction_name_2[i]
       prob.i <- prob[,,i]
-      netVisual_circle(prob.i, top = 1, color.use = color.use, vertex.size = vertex.size, signaling.name = signalName_i)
+      netVisual_circle(prob.i, from = from, to = to, bidirection = bidirection, remove.isolate = remove.isolate, top = 1, color.use = color.use, vertex.size = vertex.size, signaling.name = signalName_i)
     }
 
   }
 }
-
 
 
 
@@ -559,6 +568,10 @@ netVisual_hierarchy2 <-function(net, vertex.receiver, color.use = NULL, signalin
 #'
 #' @param net A weighted matrix representing the connections
 #' @param color.use Colors represent different cell groups
+#' @param from a vector giving the index or the name of source cell groups
+#' @param to a vector giving the index or the name of target cell groups.
+#' @param bidirection whether show the bidirectional communication, i.e., both 'from'->'to' and 'to'->'from'.
+#' @param remove.isolate whether remove the isolate nodes in the communication network
 #' @param signaling.name the name of the title
 #' @param top the fraction of interactions to show
 #' @param weight.scale whether scale the weight
@@ -595,11 +608,58 @@ netVisual_hierarchy2 <-function(net, vertex.receiver, color.use = NULL, signalin
 #' @import igraph
 #' @return A network graph of the significant interactions
 #' @export
-netVisual_circle <-function(net, color.use = NULL,signaling.name = NULL, top = 1, weight.scale = FALSE,label.edge = FALSE,edge.curved=0.2,shape='circle',layout=in_circle(),vertex.size=20,margin=0.2,
+netVisual_circle <-function(net, color.use = NULL, from = NULL, to = NULL, bidirection = FALSE, remove.isolate = FALSE, signaling.name = NULL, top = 1, weight.scale = FALSE,label.edge = FALSE,edge.curved=0.2,shape='circle',layout=in_circle(),vertex.size=20,margin=0.2,
                             vertex.label.cex=0.8,vertex.label.color= "black",arrow.width=1,arrow.size = 0.2,edge.label.color='black',edge.label.cex=0.5,edge.max.width=8,alpha.edge = 0.6){
   options(warn = -1)
   thresh <- stats::quantile(net, probs = 1-top)
   net[net < thresh] <- 0
+
+  if (!is.null(from)) {
+    if (!is.numeric(from)) {
+      group.names.all <- rownames(net)
+      if (is.null(group.names.all)) {
+        stop("No rownames exist in the input network matrix. Please define 'from' using numeric vector. ")
+      }
+      from <- match(from, group.names.all)
+      if (sum(is.na(from)) > 0) {
+        message("Some input cell group names in 'from' do not exist!")
+        from <- from[!is.na(from)]
+      }
+    }
+    if (is.null(to)) {
+      to <- 1:ncol(net)
+    } else {
+      if (!is.numeric(to)) {
+        to <- match(to, group.names.all)
+        if (sum(is.na(to)) > 0) {
+          message("Some input cell group names in 'to' do not exist!")
+          to <- to[!is.na(to)]
+        }
+      }
+    }
+    if (bidirection) {
+      from2 <- c(from, to)
+      to2 <- c(to, from)
+      from.c <- setdiff(1:nrow(net), from2)
+      to.c <- setdiff(1:nrow(net), to2)
+      net[from.c, ] <- 0
+      net[ ,to.c] <- 0
+      net[from, from] <- 0
+      net[to, to] <- 0
+    } else {
+      from.c <- setdiff(1:nrow(net), from)
+      to.c <- setdiff(1:nrow(net), to)
+      net[from.c, ] <- 0
+      net[ ,to.c] <- 0
+    }
+  }
+  if (remove.isolate) {
+    idx1 <- which(Matrix::rowSums(net) == 0)
+    idx2 <- which(Matrix::colSums(net) == 0)
+    idx <- intersect(idx1, idx2)
+    net <- net[-idx, ]
+    net <- net[, -idx]
+  }
   g <- graph_from_adjacency_matrix(net, mode = "directed", weighted = T)
   edge.start <- igraph::ends(g, es=E(g), names=FALSE)
   coords<-layout_(g,layout)
@@ -793,8 +853,9 @@ netVisual_bubble <- function(object, from, to, bidirection = FALSE, pairLR.use0 
     stop("The length of 'from' and 'to' must be the same!")
   }
   if (bidirection) {
-    from <- c(from, to)
+    from2 <- c(from, to)
     to <- c(to, from)
+    from <- from2
   }
   if (is.null(pairLR.use0)) {
     k <- 0
