9726dc60bd5c33f79c4e9e9b36dc5d098584ff04
Suoqin Jin <32399212+sqjin@users.noreply.github.com>
2021-12-13 20:19:34 +0800

    Add database for zebrafish

--- R/data.R
+++ R/data.R
@@ -14,6 +14,14 @@
 #' @source \url{https://github.com/sqjin/CellChat/}
 "CellChatDB.human"
 
+#' Ligand-receptor interactions in CellChat database for Zebrafish
+#'
+#' The ligand-receptor interaction database curated in CellChat tool
+#'
+#' @format A list includes the ligand-receptor interactions
+#' @source \url{https://github.com/sqjin/CellChat/}
+"CellChatDB.zebrafish"
+
 #' Mouse Protein-Protein interactions
 #'
 #' An adjacency matrix of the high confidence known experimental interactions
